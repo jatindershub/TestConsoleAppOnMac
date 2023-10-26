301da5e5b6bdb377bfc598d4c57456acbda1ea9a
jatindershub <66969129+jatindershub@users.noreply.github.com>
2023-10-26 16:28:34 +0200

    arbejde

--- Program.cs
+++ Program.cs
@@ -34,7 +34,8 @@ void Metode2()
     
 }*/
 
-try
+/*try
+
 {
     Console.WriteLine("Indtast et tal");
     int userInput = Convert.ToInt32(Console.ReadLine());   
@@ -43,3 +44,77 @@ catch (Exception e)
 {
     Console.WriteLine("Du har ikke indtastet et tal. Fejlbesked: " + e.Message);
 }
+catch (Exception e)
+{
+    Console.WriteLine("Du har ikke indtastet et tal. Fejlbesked: " + e.Message);
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StringMatcher
+{
+    public int CountMatchingWords(string valA, string valB, Dictionary<string, string> replacements)
+    {
+        valA = ReplaceSpecialCharacters(valA.ToLowerInvariant(), replacements);
+        valB = ReplaceSpecialCharacters(valB.ToLower(), replacements);
+
+        var wordsA = valA.Split(' ').ToList();
+        var wordsB = valB.Split(' ').ToList();
+
+        return wordsA.Intersect(wordsB).Count();
+    }
+
+    public string ReplaceSpecialCharacters(string input, Dictionary<string, string> replacements)
+    {
+        foreach (var pair in replacements)
+        {
+            input = input.Replace(pair.Key, pair.Value);
+        }
+
+        return input;
+    }
+
+    public bool FirstWordContainsBindestreg(string val)
+    {
+        var firstWord = val.Split(' ').FirstOrDefault();
+        return firstWord != null && firstWord.Contains("-");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var matcher = new StringMatcher();
+
+        string ValA = "Per-Ole Andre Nygaard Falster Jensen";
+        string ValB = "Per-Ole André Simon Ñygård";
+
+        var replacements = new Dictionary<string, string>
+        {
+            { "aa", "å" },
+            { "ae", "æ" },
+            { "ñ", "n" },
+            { "é", "e" },
+            { "-", " " }
+        };
+
+        int matchingWordsCount = matcher.CountMatchingWords(ValA, ValB, replacements);
+
+        string newValA = matcher.ReplaceSpecialCharacters(ValA.ToLower(), replacements);
+        string newValB = matcher.ReplaceSpecialCharacters(ValB.ToLower(), replacements);
+
+        bool starterMedBindestregA = matcher.FirstWordContainsBindestreg(ValA);
+        bool starterMedBindestregB = matcher.FirstWordContainsBindestreg(ValB);
+
+        Console.WriteLine($"Number of matching words: {matchingWordsCount}");
+        Console.WriteLine($"newValA: {newValA}");
+        Console.WriteLine($"newValB: {newValB}");
+        Console.WriteLine($"StarterMedBindestreg for ValA: {starterMedBindestregA}");
+        Console.WriteLine($"StarterMedBindestreg for ValB: {starterMedBindestregB}");
+    }
+}
+
